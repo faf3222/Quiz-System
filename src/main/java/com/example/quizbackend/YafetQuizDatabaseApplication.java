fcd9eee02f596f787b19c4ce9500f7c028b4f2de
erwankayiranga <kayilizhu@gmail.com>
2025-12-15 02:06:34 +0100

    donee

---
+++ src/main/java/com/example/quizbackend/YafetQuizDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.quizbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YafetQuizDatabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YafetQuizDatabaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/quizbackend/YafetQuizDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quizbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YafetQuizDatabaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
